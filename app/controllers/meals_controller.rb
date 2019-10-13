451a1eb8cf63df44c5a73f22f8882a08fb52d1d1
LoneKP <lone.kjeldgaard.pedersen@gmail.com>
2019-10-13 22:53:32 +0200

    create endpoint

--- app/controllers/meals_controller.rb
+++ app/controllers/meals_controller.rb
@@ -4,4 +4,12 @@ class MealsController < ApplicationController
   def index
     render json: Meal.all.as_json
   end
+
+  def show
+    render json: Meal.find(params[:id]).as_json
+  end
+
+  def create
+  end
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
+# frozen_string_literal: true
+
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
-  resources :meals, only: %(index)
+  resources :meals, only: %i[index show create]
 end
