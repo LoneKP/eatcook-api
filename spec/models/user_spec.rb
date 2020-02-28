78ab7751ccbc9c0e8b5663c167cd507a3dd283eb
LoneKP <lone.kjeldgaard.pedersen@gmail.com>
2020-02-28 14:35:49 +0100

    test user validations

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,26 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context 'validation tests' do
+    it 'ensures name presence' do
+      user = User.new(email: 'broder@salsa.dk').save
+      expect(user).to eq(false)
+    end
+
+    it 'ensures email presence' do
+      user = User.new(name: 'Broder Salsa').save
+      expect(user).to eq(false)
+    end
+    it 'ensures password presence' do
+      user = User.new(email: 'broder@salsa.dk').save
+      expect(user).to eq(false)
+    end
+
+    it 'should save successfully' do
+      user = User.new(name: 'Broder Salsa', email: 'broder@salsa.dk', password: '12345678').save
+      expect(user).to eq(true)
+    end
+  end
 end
