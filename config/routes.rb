f2a5defc19cc081e80cc48a2bf446e3d23b337c6
LoneKP <lone.kjeldgaard.pedersen@gmail.com>
2020-03-01 20:26:15 +0100

    change routes to match new method names

--- config/routes.rb
+++ config/routes.rb
@@ -10,14 +10,14 @@ Rails.application.routes.draw do
   post 'auth/register', to: 'users#register'
   post 'auth/login', to: 'users#login'
 
-  get 'user/meals/past', to: 'meals#past_meals_handouts'
-  get 'user/meals/future', to: 'meals#future_meals_handouts'
-  get 'user/orders/past', to: 'orders#meals_you_ate'
-  get 'user/orders/future', to: 'orders#meals_you_will_eat'
+  get 'user/meals/past', to: 'meals#meals_cooked_by_user_in_the_past'
+  get 'user/meals/future', to: 'meals#meals_to_be_cooked_by_user_in_the_future'
+  get 'user/orders/past', to: 'orders#meals_ordered_by_user_in_the_past'
+  get 'user/orders/future', to: 'orders#meals_ordered_by_user_in_the_future'
   get 'user', to: 'users#show'
   post 'user/update', to: 'users#update'
   post '/image/:meal_id', to: 'meals#attach_image'
-  
+
   get 'user/meals/pickups/:id', to: 'meals#future_meal_handouts'
   get 'user/orders/pickups/:id', to: 'orders#future_order_pickups'
 end
