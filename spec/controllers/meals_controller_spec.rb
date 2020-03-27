8f1df23a6cca0b1173beddd2e2b54539899022e2
LoneKP <lone.kjeldgaard.pedersen@gmail.com>
2020-03-27 09:54:16 +0100

    dont add test

--- spec/controllers/meals_controller_spec.rb
+++ spec/controllers/meals_controller_spec.rb
@@ -2,18 +2,18 @@
 
 require 'rails_helper'
 
-RSpec.describe MealsController, type: :controller do
-  it 'sends a json object without an image' do
-    headers = { 'ACCEPT' => 'application/json' }
-    post '/widgets', params: { widget: { name: 'My Widget' } }, headers: headers
-    allow(show).to receive(:call).and_return(:data)
-  end
-  let(:response) {}
+# RSpec.describe MealsController, type: :controller do
+#   it 'sends a json object without an image' do
+#     headers = { 'ACCEPT' => 'application/json' }
+#     post '/widgets', params: { widget: { name: 'My Widget' } }, headers: headers
+#     allow(show).to receive(:call).and_return(:data)
+#   end
+#   let(:response) {}
 
-  describe 'GET show' do
-    it 'returns a 200' do
-      request.headers['Authorization'] = 'foo'
-      expect(response).to have_http_status(:ok)
-    end
-  end
-end
+#   describe 'GET show' do
+#     it 'returns a 200' do
+#       request.headers['Authorization'] = 'foo'
+#       expect(response).to have_http_status(:ok)
+#     end
+#   end
+# end
